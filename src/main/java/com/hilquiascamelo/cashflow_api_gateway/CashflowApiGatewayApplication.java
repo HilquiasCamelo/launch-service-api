1fcff33452a27580c44039496b54c87e864d63c5
HilquiasCamelo <hilquias@ciandt.com>
2024-10-23 10:38:49 -0300

    Initial commit

---
+++ src/main/java/com/hilquiascamelo/cashflow_api_gateway/CashflowApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hilquiascamelo.cashflow_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashflowApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashflowApiGatewayApplication.class, args);
+	}
+
+}
